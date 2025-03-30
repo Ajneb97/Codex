16a7fc3c9aa6a38ef1429da2cdf4c9e5acc11e12
Ajneb97 <ajneb97@gmail.com>
2025-03-30 15:25:43 -0300

    2.3.5

--- src/main/java/cx/ajneb97/Codex.java
+++ src/main/java/cx/ajneb97/Codex.java
@@ -138,6 +138,9 @@ public class Codex extends JavaPlugin {
             case "1.21.4":
                 serverVersion = ServerVersion.v1_21_R3;
                 break;
+            case "1.21.5":
+                serverVersion = ServerVersion.v1_21_R4;
+                break;
             default:
                 serverVersion = ServerVersion.valueOf(packageName.replace("org.bukkit.craftbukkit.", ""));
         }
